4a41325e28315ff0a952ff27c1f3f69e9c18641d
pranjitbis <Pranjitbiswas77@gmail.com>
2025-12-26 20:19:37 +0530

    s

--- app/Home/component/GlobalServicesMap/page.jsx
+++ app/Home/component/GlobalServicesMap/page.jsx
@@ -1,5 +1,6 @@
 import dynamic from "next/dynamic";
 
+// Dynamic import (client-only)
 const CountryMap = dynamic(() => import("./CountryMapClient"), { ssr: false });
 
 export default function Page() {
